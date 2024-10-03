82446dd98ffe512094928b845fa8f248329ebaa7
tluyben <tycho@e-lab.nl>
2024-10-03 05:55:42 +0100

    performance

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -16,15 +17,17 @@ import (
 )
 
 type Backend struct {
-	URL string `yaml:"url"`
-	Up  bool
+	URL  string `yaml:"url"`
+	Up   bool
+	Last time.Time
 }
 
 type Config struct {
-	Port     int       `yaml:"port"`
-	Interval int       `yaml:"interval"`
-	Health   string    `yaml:"health"`
-	Backends []Backend `yaml:"backends"`
+	Port           int       `yaml:"port"`
+	Interval       int       `yaml:"interval"`
+	Health         string    `yaml:"health"`
+	Backends       []Backend `yaml:"backends"`
+	HealthyBackends []int
 }
 
 var (
@@ -35,6 +38,7 @@ var (
 
 func init() {
 	flag.StringVar(&configFile, "config", "backend.yml", "Path to the backend configuration file")
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
@@ -44,6 +48,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
+
+	log.Printf("Loaded configuration: %+v", config)
 
 	go healthCheck()
 
@@ -63,7 +69,6 @@ func loadConfig() error {
 		return err
 	}
 
-	// Set default values if not specified in the config file
 	if config.Port == 0 {
 		config.Port = 80
 	}
@@ -80,24 +85,34 @@ func loadConfig() error {
 func healthCheck() {
 	for {
 		mu.Lock()
+		config.HealthyBackends = []int{}
 		for i, backend := range config.Backends {
 			backendURL, err := url.Parse(backend.URL)
 			if err != nil {
+				log.Printf("Failed to parse backend URL %s: %v", backend.URL, err)
 				config.Backends[i].Up = false
 				continue
 			}
 
 			_, err = net.LookupHost(backendURL.Hostname())
 			if err != nil {
+				log.Printf("DNS lookup failed for %s: %v", backendURL.Hostname(), err)
 				config.Backends[i].Up = false
 				continue
 			}
 
-			resp, err := http.Get(backend.URL + config.Health)
-			if err != nil || resp.StatusCode != http.StatusOK {
+			healthURL := backend.URL + config.Health
+			resp, err := http.Get(healthURL)
+			if err != nil {
+				log.Printf("Health check failed for %s: %v", healthURL, err)
 				config.Backends[i].Up = false
 			} else {
-				config.Backends[i].Up = true
+				config.Backends[i].Up = resp.StatusCode == http.StatusOK
+				if config.Backends[i].Up {
+					config.HealthyBackends = append(config.HealthyBackends, i)
+					config.Backends[i].Last = time.Now()
+				}
+				resp.Body.Close()
 			}
 		}
 		mu.Unlock()
@@ -110,28 +125,23 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	mu.RLock()
 	defer mu.RUnlock()
 
-	for _, backend := range config.Backends {
-		if !backend.Up {
-			continue
-		}
-
-		backendURL, err := url.Parse(backend.URL)
-		if err != nil {
-			continue
-		}
-
-		// Perform an additional health check
-		resp, err := http.Get(backend.URL + config.Health)
-		if err != nil || resp.StatusCode != http.StatusOK {
-			continue
-		}
-
-		// Forward the request to the backend
-		proxy := httputil.NewSingleHostReverseProxy(backendURL)
-		proxy.ServeHTTP(w, r)
+	if len(config.HealthyBackends) == 0 {
+		log.Printf("No healthy backends available, returning 502 Bad Gateway")
+		http.Error(w, "502 Bad Gateway", http.StatusBadGateway)
 		return
 	}
 
-	// If all backends are down, return a Gateway error
-	http.Error(w, "502 Bad Gateway", http.StatusBadGateway)
+	backendIndex := config.HealthyBackends[rand.Intn(len(config.HealthyBackends))]
+	backend := config.Backends[backendIndex]
+
+	backendURL, err := url.Parse(backend.URL)
+	if err != nil {
+		log.Printf("Failed to parse backend URL %s: %v", backend.URL, err)
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Proxying request to %s", backend.URL)
+	proxy := httputil.NewSingleHostReverseProxy(backendURL)
+	proxy.ServeHTTP(w, r)
 }
